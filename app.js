99036d1de66c6c48535bdaa7aede2e199cc6eb70
Fleuron Gnadekpa <vilfleur@gmail.com>
2022-07-31 16:38:28 +0200

    add mailto and clean app.js

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-const sectionElts = document.querySelectorAll("section");
-const navigationElt = document.querySelector(".controls");
-const navBtns = document.querySelectorAll(".control");
-const container = document.querySelector(".container");
-const btnTheme = document.querySelector(".btn-theme");
-
-btnTheme.addEventListener("click", function toggleTheme() {
-    document.body.classList.toggle("light")
-})
-
-function switchPage() {
-    // change active navbar button
-    navBtns.forEach(btn => {
-        btn.addEventListener("click", switchActive)
+document
+    .querySelector(".btn-theme")
+    .addEventListener("click", function toggleTheme() {
+        const themeDark = localStorage.getItem("themeDark")
+        if (!themeDark) {
+            localStorage.setItem("themeDark", "off")
+        } else {
+            localStorage.setItem("themeDark", themeDark == "on" ? "off" : "on")
+        }
+        document.body.classList.toggle("light")
     })
 
-    // section
-    // sectionElts.addEventListener("")
+function enableControls() {
+    const controlElts = document.querySelectorAll(".control")
+    controlElts.forEach(btn => {
+        btn.addEventListener("click", changeActiveSection)
+    })
+
+    function changeActiveSection() {
+        const activeBtn = document.querySelector(".control.active")
+        activeBtn.classList.remove("active")
+        this.classList.add("active")
+
+        const newActiveSection = document.getElementById(this.dataset.target)
+        const previouslyActive = document.querySelector(".section.active")
+        previouslyActive.classList.remove("active")
+        newActiveSection.classList.add("active")
+    }
 }
 
-function switchActive() {
-    const activeBtn = document.querySelector(".control.active");
-    activeBtn.classList.remove("active");
-    this.classList.add("active");
-
-    const newActiveSection = document.getElementById(this.dataset.target);
-    const previouslyActive = document.querySelector(".section.active");
-    previouslyActive.classList.remove("active");
-    newActiveSection.classList.add("active");
-}
-
-switchPage()
+enableControls()
